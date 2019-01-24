55e64fc7fc69c7afec16042ab63ccd516bb4188a
simone.rainieri <simone.rainieri@gmail.com>
2019-01-24 14:19:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artist names: "song titles" };
 
 function updatePlaylist(playlist, "artist name", song title) {
-  
+  return playlist.assign({}, playlist, { ["artist name"]: song title})
 }
